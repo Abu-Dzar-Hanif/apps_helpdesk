f7f810224e7622575c6ab192b2cc9d257c4bc525
Abu Dzar Hanif <abudzar950@gmail.com>
2022-05-19 23:58:50 +0700

    ubah query select menjadi select join teknisi on gender

--- api/data_teknisi.php
+++ api/data_teknisi.php
@@ -1,12 +1,12 @@
 <?php 
 require('../config/koneksi.php');
-$query = mysqli_query($koneksi,"SELECT * FROM tbl_teknisi");
+$query = mysqli_query($koneksi,"SELECT * FROM tbl_teknisi JOIN tbl_gender ON tbl_teknisi.gender_teknisi = tbl_gender.inisial");
 $respons = [];
 while($data = mysqli_fetch_array($query)){
     $a = [
         'id_teknisi' => $data['id_teknisi'],
         'nama_teknisi' => $data['nama_teknisi'],
-        'gender_teknisi' => $data['gender_teknisi'],
+        'gender_teknisi' => $data['jenis_gender'],
     ];
     array_push($respons, $a);
 }
